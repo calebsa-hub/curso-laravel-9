050d10f6edb9afeb95bd860b21d2ef2d23511f16
caleb.ferreira <caleb.ferreira@pede.ai>
2022-09-21 23:21:00 -0300

    Crud com laravel 9

---
+++ resources/views/users/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title', 'Listagem dos usuários')
+
+@section('content')
+<h1>
+    Listagem de usuários
+    (<a href="{{ route('users.create') }}">+</a>)
+</h1>
+<ul>
+    @foreach($users as $user)
+        <li>
+            {{ $user->name }} -
+            {{ $user->email }}
+            | <a href="{{ route('users.edit', $user->id) }}">Editar</a>
+            | <a href="{{ route('users.show', $user->id) }}">Detalhes</a>
+        </li>
+    @endforeach
+</ul>
+@endsection
