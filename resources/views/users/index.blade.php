7a64a96ad8bdfa56be51fb969c1e54281da917ff
caleb.ferreira <caleb.ferreira@pede.ai>
2022-09-22 22:58:04 -0300

    Filter users

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,6 +7,12 @@
     Listagem de usuários
     (<a href="{{ route('users.create') }}">+</a>)
 </h1>
+
+<form action="{{ route('users.index') }}" method="get">
+    <input type="text" name="search" placeholder="Pesquisar">
+    <button>Pesquisar</button>
+</form>
+
 <ul>
     @foreach($users as $user)
         <li>
