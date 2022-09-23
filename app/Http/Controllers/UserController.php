fd04391d097033280b6f1493dcb64a0cc4b0f17a
caleb.ferreira <caleb.ferreira@pede.ai>
2022-09-22 23:45:02 -0300

    Refatorar o código

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,15 +8,18 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
+
+    protected $model;
+
+    public function __construct(User $user)
+    {
+        $this->model = $user;
+
+    }
+
     public function index(Request $request)
     {
-        $search = $request->search;
-        $users = User::where(function ($query) use ($search) {
-            if ($search) {
-                $query->where('email', $search);
-                $query->orWhere('name', 'LIKE', "%{$search}%");
-            }
-        })->get();
+        $users = $this->model->getUsers(search: $request->search ?? '');
 
         return view('users.index', compact('users'));
     }
@@ -24,7 +27,7 @@ class UserController extends Controller
     public function show($id)
     {
         //$user = User::where('id',$id)->first();
-        if (!$user = User::find($id)) {
+        if (!$user = $this->model->find($id)) {
             return redirect()->route('users.index');
         }
 
@@ -41,14 +44,14 @@ class UserController extends Controller
         $data = $request->all();
         $data['password'] = bcrypt($request->password);
 
-        User::create($data);
+        $this->model->create($data);
 
         return redirect()->route('users.index');
     }
 
     public function edit($id)
     {
-        if (!$user = User::find($id)) {
+        if (!$user = $this->model->find($id)) {
             return redirect()->route('users.index');
         }
 
@@ -57,7 +60,7 @@ class UserController extends Controller
 
     public function update(StoreUpdateUserFormRequest $request, $id)
     {
-        if (!$user = User::find($id)) {
+        if (!$user = $this->model->find($id)) {
             return redirect()->route('users.index');
         }
 
@@ -75,7 +78,7 @@ class UserController extends Controller
 
     public function delete($id)
     {
-        if (!$user = User::find($id)) {
+        if (!$user = $this->model->find($id)) {
             return redirect()->route('users.index');
         }
 
